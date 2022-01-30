e3857b05986fb5ad98ba56ea063cd4eb044c7d3c
guijun13 <guilhermejun13.grillo@gmail.com>
2022-01-29 21:38:51 -0300

    feat: add global css styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,50 @@
 import type { NextPage } from 'next';
 import { ReactChild } from 'react';
+import appConfig from '../config.json';
+
+function GlobalStyle() {
+  return (
+    <style jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */
+      html,
+      body,
+      #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */
+    `}</style>
+  );
+}
 
 const Title = (props: { children: ReactChild; tag: any }) => {
   const Tag = props.tag;
   return (
     <>
       <Tag>{props.children}</Tag>
-      <style jsx>
-        {`
-          ${Tag} {
-            color: red;
-            font-size: 24px;
-            font-weight: 600;
-          }
-        `}
-      </style>
+      <style jsx>{`
+        ${Tag} {
+          color: ${appConfig.theme.colors.neutrals['900']};
+          font-size: 24px;
+          font-weight: 600;
+        }
+      `}</style>
     </>
   );
 };
@@ -22,6 +52,7 @@ const Title = (props: { children: ReactChild; tag: any }) => {
 const Homepage: NextPage = () => {
   return (
     <>
+      <GlobalStyle />
       <Title tag="h1">Welcome back!</Title>
       <h2>Finally, Concord</h2>
     </>
