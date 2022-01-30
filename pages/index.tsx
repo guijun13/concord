38289d8f088d86e7ed13817a7b8ae4f2b17dd180
guijun13 <guilhermejun13.grillo@gmail.com>
2022-01-29 21:30:18 -0300

    feat: init homepage titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
 import type { NextPage } from 'next';
+import { ReactChild } from 'react';
+
+const Title = (props: { children: ReactChild; tag: any }) => {
+  const Tag = props.tag;
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>
+        {`
+          ${Tag} {
+            color: red;
+            font-size: 24px;
+            font-weight: 600;
+          }
+        `}
+      </style>
+    </>
+  );
+};
 
 const Homepage: NextPage = () => {
   return (
     <>
-      <h1>Welcome back!</h1>
+      <Title tag="h1">Welcome back!</Title>
       <h2>Finally, Concord</h2>
     </>
   );
