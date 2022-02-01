f6d23a37ccd8a6a579c085224c5587d391b0a6c9
guijun13 <guilhermejun13.grillo@gmail.com>
2022-02-01 00:49:30 -0300

    feat: add chat messages main feature

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,7 +1,181 @@
 import { NextPage } from 'next';
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React, { ReactPropTypes, useState } from 'react';
+import appConfig from '../config.json';
 
 const Chat: NextPage = () => {
-  return <>Página do chat</>;
+  const [message, setMessage] = useState('');
+  const [messagesList, setMessagesList] = useState([]);
+
+  function handleNewMessage(newMessage: string) {
+    const message = {
+      id: messagesList.length,
+      from: 'guijun13',
+      text: newMessage,
+    };
+    setMessagesList([message, ...messagesList]);
+    setMessage('');
+  }
+
+  return (
+    <Box
+      styleSheet={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary['500'],
+        color: appConfig.theme.colors.neutrals['000'],
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          borderRadius: '5px',
+          backgroundColor: appConfig.theme.colors.neutrals['700'],
+          height: '100%',
+          maxWidth: '95%',
+          // maxHeight: '95vh',
+          padding: '32px',
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: 'relative',
+            display: 'flex',
+            flex: 1,
+            height: '80%',
+            backgroundColor: appConfig.theme.colors.neutrals['600'],
+            flexDirection: 'column',
+            borderRadius: '5px',
+            padding: '16px',
+          }}
+        >
+          <MessageList messagesList={messagesList} />
+
+          <Box
+            as="form"
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <TextField
+              name="textField"
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: '100%',
+                border: '0',
+                resize: 'none',
+                borderRadius: '5px',
+                padding: '6px 8px',
+                backgroundColor: appConfig.theme.colors.neutrals['800'],
+                marginRight: '12px',
+                color: appConfig.theme.colors.neutrals['200'],
+              }}
+              value={message}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setMessage(event.target.value);
+              }}
+              onKeyPress={(event: React.KeyboardEvent<HTMLInputElement>) => {
+                if (event.key === 'Enter') {
+                  event.preventDefault();
+                  handleNewMessage(message);
+                }
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
+
+function Header() {
+  return (
+    <>
+      <Box
+        styleSheet={{
+          width: '100%',
+          marginBottom: '16px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button variant="tertiary" colorVariant="neutral" label="Logout" href="/" />
+      </Box>
+    </>
+  );
+}
+
+function MessageList(props) {
+  console.log(props.messagesList);
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        flex: 1,
+        color: appConfig.theme.colors.neutrals['000'],
+        marginBottom: '16px',
+      }}
+    >
+      {props.messagesList.map((message) => {
+        return (
+          <Text
+            key={message.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: '5px',
+              padding: '6px',
+              marginBottom: '12px',
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              },
+            }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: '8px',
+              }}
+            >
+              <Image
+                alt="profile-pic"
+                styleSheet={{
+                  width: '20px',
+                  height: '20px',
+                  borderRadius: '50%',
+                  display: 'inline-block',
+                  marginRight: '8px',
+                }}
+                src={`https://github.com/guijun13.png`}
+              />
+              <Text tag="strong">{message.from}</Text>
+              <Text
+                styleSheet={{
+                  fontSize: '10px',
+                  marginLeft: '8px',
+                  color: appConfig.theme.colors.neutrals[300],
+                }}
+                tag="span"
+              >
+                {new Date().toLocaleDateString()}
+              </Text>
+            </Box>
+            {message.text}
+          </Text>
+        );
+      })}
+    </Box>
+  );
+}
 
 export default Chat;
